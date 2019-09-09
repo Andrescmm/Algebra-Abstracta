3155d17f2441ab0e87253a420de70aa81d21fc75
Andrescmm <49249500+Andrescmm@users.noreply.github.com>
2019-09-09 11:05:08 -0500

    Update main.cpp

--- Vernam/main.cpp
+++ Vernam/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
+#include "Vernam.h"
+using namespace std;
 
+
+
+int main(){
+    string mensaje;
+    cout<<"Ingresar las letras por teclado"<<endl;
+    cin>>mensaje;
+    int tam=mensaje.length();
+
+    Vernam emisor(tam);
+    emisor.cifrado(mensaje);
+
+    return 0;
+}
