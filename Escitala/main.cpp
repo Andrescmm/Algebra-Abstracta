4805995aad5fec818acff692673cb3aea611f5d7
Andres Cusirramos <49249500+Andrescmm@users.noreply.github.com>
2019-09-15 22:25:30 -0500

    Update main.cpp

--- Escitala/main.cpp
+++ Escitala/main.cpp
@@ -1,29 +1,30 @@
-#include<iostream>
-#include<string>
-#include<ctime>
-#include<cmath>
-#include "Vigennere.h"
-using namespace std ;
+#include <iostream>
+#include "Escitala.h"
+#include <string>
+#include <fstream>
 
+using namespace std;
 
+int main()
+{
 
-
-int main (){
-    string code="";
-    string mensaje="";
-    string clave="";
-    cout<<"Ingresa un mensaje"<<endl;
-    getline(cin,mensaje);
-    Vigennere a (mensaje.length());
-    a.Encriptado(mensaje);
-    cout<<"Ingresar el texto cifrado"<<endl;
-    getline(cin,code);
-    cout<<"Ingresar la clave"<<endl;
-    getline(cin,clave);
-    Vigennere b(clave);
-    b.Desencriptado(code);
-
-
+    string text;
+    ifstream entrada("mensaje.txt");
+    getline(entrada,text);
+    int row,column;
+    cout<<"Ingresar la cantidad de filas y columnas"<<endl;
+    int filas;
+    cin>>filas;
+    int columnas;
+    cin>>columnas;
+    Escitala a(filas,columnas);
+    string message= a.Encriptado(text);
+    cout<<message<<endl;
+    /*cout<<"Ingresar filas y columnas "<<endl;
+    cin>>row;
+    cin>>column;*/
+    Escitala b(filas,columnas);
+    b.Descifrado(message);
 
     return 0;
 }
