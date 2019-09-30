4fd55658e6d9b510702b291f9d06638a22446f2e
Andres Cusirramos <49249500+Andrescmm@users.noreply.github.com>
2019-09-29 22:31:41 -0500

    Update Enigma.h

--- Enigma/Enigma.h
+++ Enigma/Enigma.h
@@ -1,26 +1,28 @@
-#ifndef ENIGMA_ENIGMA_H
-#define ENIGMA_ENIGMA_H
 #include <iostream>
 #include <string>
 using namespace std;
 
 class Enigma {
 private:
-string clave;
+    string clave;
 public:
-    string alfa ="abcdefghijklmnopqrstuvywxz";
-    string rot1= "defglmnohijtuvywxzabckpqrs";
-    string rot2= "bmnopqcefuvywxzagjklrsthid";
-    string rot3= "pqrsznoabcdeklmvywxfgtuhij";
+    string alfa1 ="abcdefghijklmnopqrstuvywxz ";
+    string alfa2 ="abcdefghijklmnopqrstuvywxz ";
+    string alfa3 ="abcdefghijklmnopqrstuvywxz ";
+   // string rot1= "defglmnohijtuvywxz abckpqrs";
+    //string rot2= "bmnop qcefuvywxzagjklrsthid";
+   // string rot3= "pqrsznoabcdekl mvywxfgtuhij";
+    string rot1= "ndjvuayfhistzob egkrxqpmwlc";
+    string rot2= "oviqxnwafhkrlbdcuysgme zptj";
+    string rot3= "bqhemopnafsgtdvjxwkyzucl ri";
 
-    Enigma(string clave);
-    string Encriptado(string mensaje);
-    string Desencriptado(string cifrado);
+    Enigma(string clave );
+    Enigma(string clave,string rot1, string rot2,string rot3);
+    string cifrado(string mensaje);
+    string descifrado (string mensaje);
 
-
-
-    
 };
 
 
-#endif //ENIGMA_ENIGMA_H
+#endif //ENIGMA2_0_ENIGMA_H
+
