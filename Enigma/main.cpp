4b52a8e67bbdffa4a54e6eb1230489aa40053e9d
Andres Cusirramos <49249500+Andrescmm@users.noreply.github.com>
2019-09-29 22:31:14 -0500

    Update main.cpp

--- Enigma/main.cpp
+++ Enigma/main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
 #include "Enigma.h"
 using namespace std;
 
-int main() {
-    string clave;
-    string mensaje;
-cout<<"Ingresar la clave"<<endl;
-getline(cin,clave);
-Enigma a(clave);
-cout<<"Ingrese el mensaje "<<endl;
-getline(cin,mensaje);
-a.Encriptado(mensaje);
 
-return 0;
+int main() {
+
+    string mensaje;
+    string code;
+    string clave;
+    cout<<"Ingrese una clave de 3 letras"<<endl;
+    getline(cin,clave);
+    cout<<"Ingresa el mensaje"<<endl;
+    getline(cin,mensaje);
+    Enigma a(clave);
+    a.cifrado(mensaje);
+    Enigma b(clave,"ndjvuayfhistzob egkrxqpmwlc","oviqxnwafhkrlbdcuysgme zptj","bqhemopnafsgtdvjxwkyzucl ri"  );
+    cout<<"Ingresar el mensaje cifrado"<<endl;
+    getline(cin,code);
+    b.descifrado(code);
+    return 0;
 }
